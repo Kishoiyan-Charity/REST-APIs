2de5209e18be530d92c96217166bc8ba9296c5d5
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-01-30 10:16:05 +0300

    authentications

--- src/db/users.ts
+++ src/db/users.ts
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('User', UserSchema);
 
 export const getUsers = () => UserModel.find();
-export const getUserEmail = (email: string) => UserModel.findOne({email});
+export const getUserByEmail = (email: string) => UserModel.findOne({email});
 export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
     'authentication.sessionToken': sessionToken
 })
@@ -21,5 +21,8 @@ export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne
 export const getUserById = (id: string) => UserModel.findById(id);
 export const creatUser = (values:Record<string, any>) => new UserModel(values)
     .save().then((user) => user.toObject());
-export const
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({_id: id });
+export const updateUserById = (id: string, values:Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+
+
 
